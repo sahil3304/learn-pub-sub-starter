38f02267dfc768f1399b93c02fdb1d9881735082
sahil3304 <siddiquisahil038@gmail.com>
2025-03-05 15:46:59 +0530

    client changes

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -3,18 +3,72 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
-	"log"
+	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type AckType int
+
+type SimpleQueueType int
+
+const (
+	SimpleQueueDurable SimpleQueueType = iota
+	SimpleQueueTransient
 )
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	bytesBody, err := json.Marshal(val)
 	if err != nil {
-		log.Fatal("unable to marshal val")
+		return fmt.Errorf("unable to marshal value: %v", err)
 	}
-	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
-		Body:        bytesBody,
-		ContentType: "application/json",
-	})
+	return ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false, // mandatory
+		false, // immediate
+		amqp.Publishing{
+			Body:        bytesBody,
+			ContentType: "application/json",
+		},
+	)
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not create channel: %v", err)
+	}
+
+	queue, err := ch.QueueDeclare(
+		queueName,
+		simpleQueueType == SimpleQueueDurable, // durable
+		simpleQueueType != SimpleQueueDurable, // auto-delete
+		false,                                 // exclusive
+		false,                                 // no-wait
+		nil,                                   // arguments
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not declare queue: %v", err)
+	}
+
+	err = ch.QueueBind(
+		queue.Name,
+		key,
+		exchange,
+		false, // no-wait
+		nil,   // args
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("could not bind queue: %v", err)
+	}
+
+	return ch, queue, nil
 }
